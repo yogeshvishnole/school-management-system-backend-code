df6c41f69c082de194e84490a8698779c50cf704
Yogesh vishnole <yogeshvishnole@gmail.com>
2020-06-03 20:26:01 +0530

    Authorization implemented

--- controllers/authController.js
+++ controllers/authController.js
@@ -101,3 +101,12 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+exports.restrictTo = (...roles) => (req, res, next) => {
+  if (!roles.includes(req.user.role)) {
+    return next(
+      new AppError('You do not have permission to perform this action', 403)
+    );
+  }
+  next();
+};

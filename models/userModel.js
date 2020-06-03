f123757c54207e53c6e71995efe039b98f62c082
Yogesh vishnole <yogeshvishnole@gmail.com>
2020-06-03 18:18:58 +0530

    Authentication completed

--- models/userModel.js
+++ models/userModel.js
@@ -58,5 +58,14 @@ userSchema.pre('save', async function (next) {
   this.passwordConfirm = undefined;
 });
 
+// Instance method for verify password during login
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
 const User = mongoose.model('User', userSchema);
 module.exports = User;
